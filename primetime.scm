e0d717215c7cfc030305cfe6dea83c2c99dc6e97
Erik Falor (fadein) <ewfalor@gmail.com>
2014-10-03 00:00:34 -0600

    Simplify looping after state change

--- primetime.scm
+++ primetime.scm
@@ -13,25 +13,29 @@
 	  (now (inexact->exact (current-seconds))))
   (let loop ((x 1) (now now) (prev-prime 1000))
 
-	; call the C function and put the list of factors into u32factors
-	(factor-time now u32factors *MAX-SIZE*)
+	(let-syntax ((doloop
+				   (syntax-rules ()
+								 ((_ pp)
+								  (begin
+									(thread-sleep! (seconds->time (+ x start)))
+									(loop (+ 1 x) (+ 1 now) 0))))))
 
-	; the 1st elemet of u32factors is the count of prime factors
-	(let* ((n (u32vector-ref u32factors 0))
-		   (prime? (= 1 n)))
+	  ; call the C function and put the list of factors into u32factors
+	  (factor-time now u32factors *MAX-SIZE*)
 
-	  (cond ((and prime? (= prev-prime 1))
-			 (printf "~a: TWIN PRIME!!!~n" now)
-			 (thread-sleep! (seconds->time (+ x start)))
-			 (loop (+ 1 x) (+ 1 now) 0))
+	  ; the 1st elemet of u32factors is the count of prime factors
+	  (let* ((n (u32vector-ref u32factors 0))
+			 (prime? (= 1 n)))
 
-			(prime?
-			 (printf "~a: PRIME TIME!!!~n" now)
-			 (thread-sleep! (seconds->time (+ x start)))
-			 (loop (+ 1 x) (+ 1 now) 0))
+		(cond ((and prime? (= prev-prime 1))
+			   (printf "~a: TWIN PRIME!!!~n" now)
+			   (doloop 0))
 
-			(else
-			  (let ((factors (subu32vector u32factors 1 (+ 1 n))))
-				(print now ": " (u32vector->list factors))
-				(thread-sleep! (seconds->time (+ x start)))
-				(loop (+ 1 x) (+ 1 now) (+ 1 prev-prime))))))))
+			  (prime?
+				(printf "~a: PRIME TIME!!!~n" now)
+				(doloop 0))
+
+			  (else
+				(let ((factors (subu32vector u32factors 1 (+ 1 n))))
+				  (print now ": " (u32vector->list factors))
+				  (doloop (+ 1 prev-prime)))))))))

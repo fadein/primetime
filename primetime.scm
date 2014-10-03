07d140708ec536a6a9b0ce0c5857828e9c79304f
Erik Falor (fadein) <ewfalor@gmail.com>
2014-10-02 22:12:48 -0600

    thread-sleep! appears to be broken :(

--- primetime.scm
+++ primetime.scm
@@ -1,9 +1,19 @@
-(use posix)
+(use posix srfi-18 srfi-19)
 
 (define factor-time (foreign-lambda void "factor_time" unsigned-integer32))
 
-(let forever ((now (current-seconds)))
-  (factor-time now)
-  (sleep 1)
-  (forever (add1 now)))
+(let ((start (time-second (current-time))))
+  (print "start=" start)
+  (let loop ((now start))
+	(factor-time now)
+	(let ((then (seconds->time (add1 now))))
+	  (print "then=" then)
+	  (thread-sleep! then)
+	  (loop then))))
 
+; a clock with no drift:
+;(let ((start (time->seconds (current-time)))
+;	  (let loop ((x 1))
+;		(thread-sleep! (seconds->time (+ x start)))
+;		(write x)
+;		(loop (+ x 1))))
